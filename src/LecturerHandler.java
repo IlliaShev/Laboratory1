9d92c8ce82a1c4f1bd345e07d695841212f4f9ac
Shevchyk-src <illia.shevchyk@ukma.edu.ua>
2021-02-27 18:37:07 +0200

    Add handlers for classes

---
+++ src/LecturerHandler.java
@@ -0,0 +1,12 @@
+public class LecturerHandler {
+    private Lecturer[] lecturers;
+    private int counter;
+
+    public LecturerHandler(){
+        lecturers = new Lecturer[20];
+    }
+
+    public void addLecturer(Lecturer lecturer){
+
+    }
+}
