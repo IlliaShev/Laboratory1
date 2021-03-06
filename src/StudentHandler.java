1ba71332e5ec3fdba907221c68f3d6c944994ef0
Alex <alexasda051@gmail.com>
2021-03-06 13:05:12 +0200

    add comments

--- src/StudentHandler.java
+++ src/StudentHandler.java
@@ -1,34 +1,67 @@
 import utils.ConsoleRequest;
 import utils.DataInput;
 
-import java.lang.reflect.Array;
-
 public class StudentHandler {
+    /**
+     * Array with students
+     */
     private Student[] students;
+    /**
+     * Maximum size of array
+     */
     private int maximumSize;
+    /**
+     * Index of the last students
+     */
     private int currentStudent;
 
+    /**
+     * Create handler for students
+     *
+     * @param maximumSize - maximum size of the array
+     */
     public StudentHandler(int maximumSize) {
         this.maximumSize = maximumSize;
         students = new Student[maximumSize];
     }
 
+    /**
+     * Create default students handler
+     */
     public StudentHandler() {
         this(10);
     }
 
+    /**
+     * Get maximum size of the faculties array
+     *
+     * @return maximum size
+     */
     public int getMaximumSize() {
         return maximumSize;
     }
 
+    /**
+     * Add student to the handler
+     *
+     * @param student - student
+     */
     public void addStudent(Student student) {
         students[currentStudent++] = student;
     }
 
+    /**
+     * Add student to the handler
+     */
     public void addStudent() {
         students[currentStudent++] = buildStudent();
     }
 
+    /**
+     * Edit student
+     *
+     * @param studentIndex - student index
+     */
     public void editStudent(int studentIndex) {
         if (ConsoleRequest.askUserTF("Бажаєте змінити ім'я студента?"))
             students[studentIndex].setName(getValidName());
@@ -38,6 +71,11 @@ public class StudentHandler {
             students[studentIndex].setCourse(getValidCourse());
     }
 
+    /**
+     * Delete student from the handler
+     *
+     * @param studentIndex - student index
+     */
     public void deleteStudent(int studentIndex) {
         Student[] newStudents = new Student[maximumSize];
         System.arraycopy(students, 0, newStudents, 0, studentIndex);
@@ -46,12 +84,18 @@ public class StudentHandler {
         currentStudent--;
     }
 
+    /**
+     * Print to the console all the student that was added
+     */
     public void showStudents() {
         for (int i = 0; i < currentStudent; i++) {
             System.out.println((i + 1) + ": " + students[i]);
         }
     }
 
+    /**
+     * Create new student
+     */
     private Student buildStudent() {
         String name = getValidName();
         String group = getValidGroup();
@@ -59,14 +103,30 @@ public class StudentHandler {
         return new Student(name, group, course);
     }
 
+    /**
+     * Get valid name student
+     *
+     * @return valid name
+     */
     private String getValidName() {
         return DataInput.getString("Введіть ім'я студента");
     }
 
+    /**
+     * Get valid group student
+     *
+     * @return valid group
+     */
     private String getValidGroup() {
         return DataInput.getString("Введіть групу студента");
     }
 
+    /**
+     * Get array students by course
+     *
+     * @param course - course
+     * @return array students by course
+     */
     public Student[] getStudentsByCourse(int course) {
         int arrSize = 0;
         for (int i = 0; i < currentStudent; i++) {
@@ -82,6 +142,11 @@ public class StudentHandler {
         return res;
     }
 
+    /**
+     * Get valid course student
+     *
+     * @return valid course
+     */
     private int getValidCourse() {
         int course = DataInput.getInt("Введіть курс студента");
         while (course < 1 || course > 6) {
@@ -89,6 +154,19 @@ public class StudentHandler {
             course = DataInput.getInt("Введіть курс студента");
         }
         return course;
+    }
+
+    /**
+     *
+     * @return information about new students
+     */
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < currentStudent; i++) {
+            res.append(i + 1).append(": ").append(students[i]).append("\n");
+        }
+        return res.toString();
     }
 
 }
