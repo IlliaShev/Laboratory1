60cafb7c2047ae4ab5c757ff5e4c7b049b2b620e
Alex <alexasda051@gmail.com>
2021-03-06 12:34:01 +0200

    Create class Faculty

--- src/Faculty.java
+++ src/Faculty.java
@@ -1,2 +1,39 @@
 public class Faculty {
+
+    /**
+     * Faculty name
+     */
+    private String name;
+
+    /**
+     * Create faculty
+     * @param name - faculty name
+     */
+    public Faculty(String name){
+        this.name = name;
+    }
+
+    /**
+     * Return faculty name
+     *
+     * @return faculty name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Change faculty name
+     *
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Факультет: " +
+                "Назва - " + name;
+    }
 }
